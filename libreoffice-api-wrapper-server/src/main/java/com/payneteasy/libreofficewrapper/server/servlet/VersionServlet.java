b1136c96425fc53ddbb5a03ca105a14790edc072
aprozorov <kjlvbnfghrty212vyt>
2020-02-12 13:43:21 +0300

    Update jetty version. Impl correct servlet's version.

--- libreoffice-api-wrapper-server/src/main/java/com/payneteasy/libreofficewrapper/server/servlet/VersionServlet.java
+++ libreoffice-api-wrapper-server/src/main/java/com/payneteasy/libreofficewrapper/server/servlet/VersionServlet.java
@@ -1,7 +1,16 @@
 package com.payneteasy.libreofficewrapper.server.servlet;
 
-import java.io.IOException;
+import com.payneteasy.libreofficewrapper.server.Main;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +19,14 @@ public class VersionServlet extends HttpServlet {
 
     @Override
     protected void doGet(final HttpServletRequest aRequest, final HttpServletResponse aResponse) throws IOException {
-        aResponse.getWriter().print(VersionServlet.class.getPackage().getImplementationVersion());
+        try (final InputStream resources = Main.class.getResourceAsStream("/META-INF/maven/com.payneteasy/libreoffice-api-wrapper-server/pom.properties")) {
+            if (resources != null) {
+                final Properties properties = new Properties();
+                properties.load(resources);
+                aResponse.getWriter().print(properties.getProperty("version"));
+            } else {
+                aResponse.sendError(500);
+            }
+        }
     }
 }
